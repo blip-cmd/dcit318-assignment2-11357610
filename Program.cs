14800ad769506e6c4b434cf5499da3f2c116a887
blip <ryanniiakweibrown1@gmail.com>
2025-08-07 02:20:15 +0000

    Inheritance and Method Overriding

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("dcit318-assignment2-11357610");
+﻿namespace Assignment2;
+public class Animal
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("I'm a goat, i'm a goat, merhhh...");
+    }
+}
+
+public class Dog : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        new Animal().MakeSound();   // I'm a goat, i'm a goat, merhhh...
+        new Dog().MakeSound();      // Bark
+        new Cat().MakeSound();      // Meow
+    }
+}
