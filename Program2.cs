c65d3e4264d0827dd7018c77793b60e48ee0a360
blip <ryanniiakweibrown1@gmail.com>
2025-08-10 12:07:38 +0000

    Define an interface called IMovable with a method Move().

---
+++ Program2.cs
@@ -0,0 +1,28 @@
+﻿using System;   
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dcit318_assignment2_11357610
+{
+    internal class Program2
+    {
+        public static void Main(string[] args)
+        {
+            interface IMoveable
+        {
+            void Move();
+
+        }
+
+        }
+
+
+
+    }         
+
+ }
+  
+
+
